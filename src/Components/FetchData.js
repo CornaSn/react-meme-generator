dfb305f0458fa8608f0eda1421e9ed37c72beb44
CornaSn <cornelia.schenk@hotmail.com>
2024-05-22 14:09:21 +0200

    Fix fetching code

--- src/Components/FetchData.js
+++ src/Components/FetchData.js
@@ -11,7 +11,7 @@ export default function FetchData(props) {
         props.setMemesArray(data);
       })
       .catch((err) => console.log(err));
-  });
+  }, []);
 
   // // Check fetch data
   // props.memesArray.length > 0
